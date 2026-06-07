96a729dd9264a3928c138c2d8134546ba355d9ac
Math Campbell <math.campbell@gmail.com>
2026-06-07 20:10:45 +0100

    Add five day forecast model

--- src/WeatherManager.h
+++ src/WeatherManager.h
@@ -9,6 +9,8 @@
 #include <stdbool.h>
 #include "Tide.h"
 
+static constexpr uint8_t WEATHER_FORECAST_DAYS = 5;
+
 struct WeatherData {
     String temperature;
     String condition;
@@ -18,7 +20,17 @@ struct WeatherData {
     String wind_speed;
     String humidity;
     String moonphase;
+    String temp_min;
+    String temp_max;
     unsigned long lastUpdate;
+    uint16_t id;
+    unsigned long dt;
+};
+
+struct WeatherForecastDay {
+    String dayLabel;
+    String temperature;
+    String icon;
     uint16_t id;
     unsigned long dt;
 };
@@ -33,15 +45,20 @@ extern WeatherData currentWeatherData;
 void WeatherManagerBegin();
 bool WeatherUpdate();
 bool WeatherLoadCached();
+bool ForecastLoadCached();
 bool TideLoadCached();
 const WeatherData& WeatherGet();
+const WeatherForecastDay* WeatherForecastGet(uint8_t& outCount);
 
 void WeatherInit();
 void printCurrentWeather();
 void updateWeatherData();
+void updateForecastData();
 const char* getMeteoconIcon(uint16_t id, bool today);
 bool loadWeatherDataFromFile(const char* filePath, WeatherData& weather);
 void saveWeatherDataToFile(const char* filePath, const WeatherData& weather);
+bool loadForecastDataFromFile(const char* filePath);
+void saveForecastDataToFile(const char* filePath);
 void initializeWeatherData();
 bool WeatherConsumeNtpSync(time_t *outEpoch);
 

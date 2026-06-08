03ed167e9c3a79d73b3ff3784050c7ca9ca7eb04
Math Campbell <math.campbell@gmail.com>
2026-06-08 21:43:58 +0100

    minor change - fixed name to Ætherwatch

--- src/ui_events.cpp
+++ src/ui_events.cpp
@@ -8,7 +8,7 @@
 #include <Arduino.h>
 #include "uiWeatherScreen.h"
 
-static constexpr const char* MAIN_DEFAULT_TITLE = "Aetherwatch";
+static constexpr const char* MAIN_DEFAULT_TITLE = "Ætherwatch";
 
 void ui_MainScreen_reset_menu_default(void)
 {
